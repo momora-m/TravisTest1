c7c86dc1bc16cbbe87b4c105dcbf43c1e59af50c
momora-m <kai10mishina@gmail.com>
2021-03-07 23:12:32 +0900

    add name2

--- src/main/java/CItest/Profile.java
+++ src/main/java/CItest/Profile.java
@@ -6,10 +6,15 @@ import java.util.List;
 
 public class Profile {
 	private String name = "リロイ・ジェンキンス";
+	private String name2 = "リロォォォォイ";
 	private List<String> tags = new ArrayList<>();
 
 	public String getName() {
 		return name;
+	}
+
+	public String getName2() {
+		return name2;
 	}
 
 	public void addTag(String tag) {
